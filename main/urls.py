deadb70b1a95829cd1eacc2eef34f5df5115cefd
KyzaevIlyas576 <kyzaevilyas@gmail.com>
2026-06-20 18:37:46 +0600

    Выполнил контрольную работу

--- main/urls.py
+++ main/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('shop.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
